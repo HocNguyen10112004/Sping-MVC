212a7400eaf0bb591dbcb6386599d23f8fa85fc9
Hoc <hocnguyen10112004@gmail.com>
2024-08-11 22:28:50 +0700

    create form

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,36 +2,50 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
-
-
 
 @Controller
 public class UserController {
+
     private UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @RequestMapping("/")
-    public String GetHomePage(Model model){
-        String test=this.userService.HandleHello();
+    public String GetHomePage(Model model) {
+        String test = this.userService.HandleHello();
         model.addAttribute("eric", test);
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String GetUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String CreateUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
+    }
+
 }
 // @RestController
 // public class UserController {
 //     private UserService userService;
-    
+
 //     public UserController(UserService userService) {
 //         this.userService = userService;
 //     }
-
 //     @GetMapping("")
-    
 //     public String GetHomePage(){
 //         return "hello from Controller";
 //     }
