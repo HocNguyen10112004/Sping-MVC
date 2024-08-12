c9a4d0e9865f4e7ffae9c8883c9c893aa2260833
Hoc <hocnguyen10112004@gmail.com>
2024-08-12 18:13:15 +0700

    save user

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -12,16 +12,17 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String GetHomePage(Model model) {
-        String test = this.userService.HandleHello();
-        model.addAttribute("eric", test);
+
+        model.addAttribute("eric", "test");
         return "hello";
     }
 
@@ -34,6 +35,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String CreateUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
         System.out.println("run here" + hoidanit);
+        this.userService.HandleSaveUser(hoidanit);
         return "hello";
     }
 
--- 01-java-spring-laptopshop-starter/src/main/webapp/WEB-INF/view/admin/user/create.jsp
+++ 01-java-spring-laptopshop-starter/src/main/webapp/WEB-INF/view/admin/user/create.jsp
@@ -1,10 +1,10 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
         <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
-    
+
 
         <html lang="en">
-a
+        
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
